706d869115ee489e5e6d062473fe284ec49e29bf
sofyanfirdaus <13521083@std.stei.itb.ac.id>
2022-10-03 09:27:40 +0700

    fix problem

--- src/main/java/tubes/algeo/Main.java
+++ src/main/java/tubes/algeo/Main.java
@@ -1,120 +1,15 @@
 package tubes.algeo;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import tubes.algeo.studikasus.BicubicInterpolation;
-import tubes.algeo.studikasus.MultipleLinearRegression;
-import tubes.algeo.studikasus.PolynomInterpolation;
-
-import static java.util.Map.entry;
-
 /**
  * Hello world!
  *
  */
 public class Main {
   public static void main( String[] args ) {
-    // Matrix m = Matrix.from(new double[][] {
-    //   {1, 2, 3, 0, 3, 5},
-    //   {5, 2, 1, 1, 0, 2},
-    //   {6, 8, 3, 2, 1, 7},
-    //   {0, 0, 2, 8, 1, 10},
-    //   {7, 4, 7, 0, 2, 16}
-    // });
-    // m.getEchelon().print();
-    // m.getReductedEchelon().print();
-
-    // Expr a = new Expr(-4, Expr.var("x", 2), Expr.var("y", -2));
-    // Expr b = new Expr(2, Expr.var("x", 1), Expr.var("z", 2));
-
-    // final HashMap<String, Expr> valueMap = new HashMap(Map.ofEntries(
-    //   entry("y", new Expr(2, Expr.var("t", 2)))
-    // ));
-    // System.out.println(a + " = " + b);
-    // System.out.println(LinearEquationSolver.solve(a, b, "x", valueMap));
-    // System.out.println(Expr.multiply(a, 2));
-    Matrix system = Matrix.from(new double[][] {
-    {1   , 1/2d , 1/3d , 1/4d , 1/5d  ,  1/6d , 1},
-    {1/2d , 1/3d , 1/4d , 1/5d , 1/6d  ,  1/7d , 0},
-    {1/3d , 1/4d , 1/5d , 1/6d , 1/7d  ,  1/8d , 0},
-    {1/4d , 1/5d , 1/6d , 1/7d , 1/8d  ,  1/9d , 0},
-    {1/5d , 1/6d , 1/7d , 1/8d , 1/9d  , 1/10d , 0},
-    {1/6d , 1/7d , 1/8d , 1/9d , 1/10d , 1/11d , 0}
-    });
-    HashMap<String, Expr> solution1 = LinearEquationSolver.solveSystemGauss(system);
-    HashMap<String, Expr> solution2 = LinearEquationSolver.solveSystemGaussJordan(system);
-    HashMap<String, Expr> solution3 = LinearEquationSolver.solveSystemCramer(system);
-    HashMap<String, Expr> solution4 = LinearEquationSolver.solveSystemInverse(system);
-    System.out.println(solution1);
-    System.out.println(solution2);
-    System.out.println(solution3);
-    System.out.println(solution4);
-    System.out.println(system);
-
-    Expr testEval = new Expr(2,
-      Expr.var("x", 1, 2),
-      Expr.var("y", 2)
-    );
-    System.out.println(testEval);
-    HashMap<String, Double> varValues = new HashMap<String, Double>(Map.ofEntries(
-      entry("x", 2d),
-      entry("y", 5d)
-    ));
-    // x^2 + 2y + 2 = 2^2 + 2*5 + 2 = 16
-    System.out.println(testEval.evaluate(varValues));
-    System.out.println(Expr.add(testEval, new Expr(-3, Expr.var("x", 3))));
-
-    PolynomInterpolation polin = new PolynomInterpolation(
-      new double[] {1, 4, 12, 15},
-      new double[] {1, 20, 45, 56}
-    );
-    System.out.println(polin.getFunction());
-    System.out.println(polin.apply(20));
-
-    Matrix bicubic = Matrix.from(new double[][] {
-    {1  ,  2 , 3  ,  4},
-    {5  ,  6 , 7  ,  8},
-    {9  , 10 , 11 , 12},
-    {13 , 14 , 15 , 16}
-    });
-    BicubicInterpolation bicin = new BicubicInterpolation(bicubic);
-    System.out.println(bicin.getFunction());
-    System.out.println(bicin.apply(0.5, 0.5));
-
-
-    Matrix system2 = Matrix.from(new double[][] {
-    {0      ,   0 , 0      ,   0 , 0      ,    0 , 1      ,   1 , 1      ,    13},
-    {0      ,   0 , 0      ,   1 , 1      ,    1 , 0      ,   0 , 0      ,    15},
-    {1      ,   1 , 1      ,   0 , 0      ,    0 , 0      ,   0 , 0      ,     8},
-    {0      ,   0 , .04289 ,   0 , .04289 , 0.75 , .04289 , .75 , .61396 , 14.79},
-    {0      , .25 , .91421 , .25 , .91421 ,  .25 , .91421 , .25 , 0      , 14.31},
-    {.61396 , .75 , .04289 , .75 , .04289 ,    0 , .04289 ,   0 , 0      ,  3.81},
-    {0      ,   0 , 1      ,   0 , 0      ,    1 , 0      ,   0 , 1      ,    18},
-    {0      ,   1 , 0      ,   0 , 1      ,    0 , 0      ,   1 , 0      ,    12},
-    {1      ,   0 , 0      ,   1 , 0      ,    0 , 1      ,   0 , 0      ,     6},
-    {.04289 , .75 , .61396 ,   0 , .04289 ,  .75 , 0      ,   0 , .04289 , 10.51},
-    {.91421 , .25 , 0      , .25 , .91421 ,  .25 , 0      , .25 , .91421 , 16.13},
-    {.04289 ,   0 , 0      , .75 , 0      ,    0 , .61396 , .75 , .04289 ,  7.04}
-    });
-    System.out.println(LinearEquationSolver.solveSystemGaussJordan(system2));
-
-    Matrix X = Matrix.from(new double[][] {
-    {674  , 2175 , 4672 ,  147},
-    {217  , 2404 , 3090 , 7464},
-    {606  , 4551 , 1982 , 2000},
-    {32   , 8265 , 7657 ,  307},
-    {671  , 1648 , 4861 , 1786},
-    {392  , 2841 , 378  , 4288},
-    {4291 , 3749 , 221  , 1123},
-    {438  , 1237 , 4817 , 3777}
-    });
-    double[] y = new double[] {606,4551,1982,2000,312,483,8847,2471};
-    MultipleLinearRegression mlr = new MultipleLinearRegression(X, y);
-    System.out.println(mlr.getFunction());
-    System.out.println(mlr.apply(674, 2175, 4672, 147));
-    
     User test = new User();
+    // test.mainMenu();
+    // Matrix m = test.scanMatrix();
+    // System.out.println(m);
     test.mainMenu();
   }
 }
--- src/main/java/tubes/algeo/User.java
+++ src/main/java/tubes/algeo/User.java
@@ -6,189 +6,184 @@ import java.io.FileNotFoundException;
 
 public class User {
 
-    public void readFile(Matrix m) {
-        Scanner ketik = new Scanner(System.in);
-        int Row,Col,i,j;
+  public Matrix readFile() {
+    Matrix m = null;
+    Scanner ketik = new Scanner(System.in);
+    int Row,Col,i,j;
 
-        try {
-        System.out.print("Tulis nama filenya : ");
-        String nama = ketik.next();
-        String checkpath = "../test/";
-        File file = new File(checkpath+nama);
-        System.out.println(file.getAbsolutePath());
+    try {
+      System.out.print("Tulis nama filenya : ");
+      String nama = ketik.next();
+      String checkpath = "../test/";
+      File file = new File(checkpath+nama);
+      System.out.println(file.getAbsolutePath());
 
-        Scanner CountRow = new Scanner(file);
-        Row = 0;
-        while (CountRow.hasNextLine()){
-            CountRow.nextLine();
-            Row += 1;
-        }
-        CountRow.close();
+      Scanner CountRow = new Scanner(file);
+      Row = 0;
+      while (CountRow.hasNextLine()){
+        CountRow.nextLine();
+        Row += 1;
+      }
+      CountRow.close();
 
-        Scanner CekBaris = new Scanner(file);
-        Col = 0;
-        if (CekBaris.hasNextLine()){
-            Scanner CountCol = new Scanner(CekBaris.nextLine());
-            while (CountCol.hasNextDouble()){
-            CountCol.nextDouble();
-            Col += 1;
-            }
-            CountCol.close();
+      Scanner CekBaris = new Scanner(file);
+      Col = 0;
+      if (CekBaris.hasNextLine()){
+        Scanner CountCol = new Scanner(CekBaris.nextLine());
+        while (CountCol.hasNextDouble()){
+          CountCol.nextDouble();
+          Col += 1;
         }
-        CekBaris.close();
-        m = new Matrix(Row,Col);
+        CountCol.close();
+      }
+      CekBaris.close();
+      m = new Matrix(Row,Col);
 
-        Scanner MatrixRow = new Scanner(file);
-        i = 0;
-        while (MatrixRow.hasNextLine()){
-            Scanner MatrixCol = new Scanner(MatrixRow.nextLine());
-            j = 0;
-            while (MatrixCol.hasNextDouble()){
-            m.setElement(i,j,MatrixCol.nextDouble());
-            j += 1;
-            }
-            MatrixCol.close();
-            i += 1;
+      Scanner MatrixRow = new Scanner(file);
+      i = 0;
+      while (MatrixRow.hasNextLine()){
+        Scanner MatrixCol = new Scanner(MatrixRow.nextLine());
+        j = 0;
+        while (MatrixCol.hasNextDouble()){
+          m.setElement(i,j,MatrixCol.nextDouble());
+          j += 1;
         }
-        MatrixRow.close();
-        System.out.println("Matrix yang terbaca dari file adalah :");
-        System.out.println(m);
-        }
-        catch (FileNotFoundException e) {
-        System.out.println("File tidak ada atau nama yang dimasukkan salah.");
-        e.printStackTrace();
-        ketik.close();
-        System.exit(0);
-        }
-        ketik.close();
+        MatrixCol.close();
+        i += 1;
+      }
+      MatrixRow.close();
+      System.out.println("Matrix yang terbaca dari file adalah :");
+      System.out.println(m);
+    }
+    catch (FileNotFoundException e) {
+      System.err.println("File tidak ada atau nama yang dimasukkan salah.");
+      e.printStackTrace();
+      ketik.close();
+      System.exit(0);
+    }
+    ketik.close();
+    return m;
+  }
+
+  public Matrix scanMatrix() {
+    int m,n;
+    System.out.println("Masukkan Ordo Matriks m x n");
+    System.out.print("Masukkan baris m : ");
+    m = input.nextInt();
+    System.out.print("Masukkan kolom n : ");
+    n = input.nextInt();
+
+    Matrix matrix = new Matrix(m,n); //Bingung implementasinya gmn
+
+    System.out.println("Tulis Elemen Matriks ");
+    for(int i = 0; i < m; i++){
+      for (int j = 0; j < n; j++){
+        matrix.setElement(i,j,input.nextDouble());
+      }
+    }
+    System.out.println("Matriks yang Anda masukkan ialah :");
+    System.out.println(matrix);
+
+    return matrix;
+  }
+
+  static Scanner input = new Scanner(System.in);
+
+  public void mainMenu() {
+    System.out.println("Tugas Besar 1 IF2123 Aljabar Linear dan Geometri");
+    System.out.println("Sistem Persamaan Linier, Determinan, dan Aplikasinya");
+    System.out.println("");
+    System.out.println("Dibuat oleh :");
+    System.out.println("13521075 - Muhammad Rifko Favian");
+    System.out.println("13521083 - Moch. Sofyan Firdaus");
+    System.out.println("13521098 - Fazel Ginanda");
+    System.out.println("");
+
+    int pil = tanyaMenuUtama(input);
+
+    while (pil != 1 && pil != 2 && pil != 3 && pil != 4 && pil != 5 && pil != 6 && pil != 7) {
+      System.out.println("");
+      System.out.println("Masukan Anda Salah! Silakan Ulangi Masukan Anda");
+      pil = tanyaMenuUtama(input);
+    }
+    System.out.println("");
+    if (pil == 1) {
+      menuSPL();
+    } else if (pil == 2) {
+      menuDeterminan();
+    } else if (pil == 3) {
+      menuMatriksBalikan();
+    } else if (pil == 4) {
+      menuInterPolinom();
+    } else if (pil == 5) {
+      menuInterBicubic();
+    } else if (pil == 6) {
+      menuRegresi();
+    } else if (pil == 7) {
+      input.close();
+      System.exit(0);
+    }
+  }
+
+  public void menuSPL() {
+    System.out.println("Anda memilih menu Sistem Persamaan Linier");
+    System.out.println("");
+    System.out.println("Sekarang Anda akan memasukkan Matriks");
+    System.out.println("Pilih metode pembacaan Matriks Anda (1. File ; 2. Keyboard)");
+    System.out.print("Masukkan pilihan Anda : ");
+    int pil;
+    pil = input.nextInt();
+    System.out.println("");
+
+    while (pil != 1 && pil != 2) {
+      System.out.println("Masukan Anda Salah! Silakan Ulangi Masukan Anda");
+      System.out.println("Pilih metode pembacaan Matriks Anda (1. File ; 2. Keyboard)");
+      System.out.print("Masukkan pilihan Anda : ");
+      pil = input.nextInt();
+      System.out.println("");
     }
 
-    public void writeMatrix(Matrix matrix) {
-        int m,n;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Masukkan Ordo Matriks m x n");
-        System.out.print("Masukkan baris m : ");
-        m = scanner.nextInt();
-        System.out.print("Masukkan kolom n : ");
-        n = scanner.nextInt();
-
-        matrix = new Matrix(m,n); //Bingung implementasinya gmn
-
-        System.out.println("Tulis Elemen Matriks ");
-        for(int i = 0; i < m; i++){
-			for (int j = 0; j < n; j++){
-				matrix.setElement(i,j,scanner.nextDouble());
-			}
-		}
-        System.out.println("Matriks yang Anda masukkan ialah :");
-        System.out.println(matrix);
-        scanner.close();
+    Matrix Maug = null;
+    if (pil == 1) {
+      Maug = readFile();
+    } else if (pil == 2) {
+      Maug = scanMatrix();
     }
+    System.out.println(Maug);
+  }
 
-    static Scanner input = new Scanner(System.in);
+  public void menuDeterminan() {
 
-    public void mainMenu() {
-        System.out.println("Tugas Besar 1 IF2123 Aljabar Linear dan Geometri");
-        System.out.println("Sistem Persamaan Linier, Determinan, dan Aplikasinya");
-        System.out.println("");
-        System.out.println("Dibuat oleh :");
-        System.out.println("13521075 - Muhammad Rifko Favian");
-        System.out.println("13521083 - Moch. Sofyan Firdaus");
-        System.out.println("13521098 - Fazel Ginanda");
-        System.out.println("");
-        System.out.println("MENU");
-        System.out.println("1. Sistem Persamaan Linier");
-        System.out.println("2. Determinan");
-        System.out.println("3. Matriks Balikan");
-        System.out.println("4. Interpolasi Polinom");
-        System.out.println("5. Interpolasi Bicubic");
-        System.out.println("6. Regresi Linear Berganda");
-        System.out.println("7. Keluar");
-        System.out.print("Masukkan pilihan menu yang diinginkan : ");
+  }
 
-        int pil;
-        pil = input.nextInt();
+  public void menuMatriksBalikan() {
 
-        while (pil != 1 && pil != 2 && pil != 3 && pil != 4 && pil != 5 && pil != 6 && pil != 7) {
-            System.out.println("");
-            System.out.println("Masukan Anda Salah! Silakan Ulangi Masukan Anda");
-            System.out.println("MENU");
-            System.out.println("1. Sistem Persamaan Linier");
-            System.out.println("2. Determinan");
-            System.out.println("3. Matriks Balikan");
-            System.out.println("4. Interpolasi Polinom");
-            System.out.println("5. Interpolasi Bicubic");
-            System.out.println("6. Regresi Linear Berganda");
-            System.out.println("7. Keluar");
-            System.out.print("Masukkan pilihan menu yang diinginkan : ");
-            pil = input.nextInt();
-        }
-        System.out.println("");
-        if (pil == 1) {
-            menuSPL();
-        } else if (pil == 2) {
-            menuDeterminan();
-        } else if (pil == 3) {
-            menuMatriksBalikan();
-        } else if (pil == 4) {
-            menuInterPolinom();
-        } else if (pil == 5) {
-            menuInterBicubic();
-        } else if (pil == 6) {
-            menuRegresi();
-        } else if (pil == 7) {
-            System.exit(0);
-        }
-    }
+  }
 
-    public void menuSPL() {
-        System.out.println("Anda memilih menu Sistem Persamaan Linier");
-        System.out.println("");
-        System.out.println("Sekarang Anda akan memasukkan Matriks");
-        System.out.println("Pilih metode pembacaan Matriks Anda (1. File ; 2. Keyboard)");
-        System.out.print("Masukkan pilihan Anda : ");
-        int pil;
-        pil = input.nextInt();
-        System.out.println("");
+  public void menuInterPolinom() {
 
-        while (pil != 1 && pil != 2) {
-            System.out.println("Masukan Anda Salah! Silakan Ulangi Masukan Anda");
-            System.out.println("Pilih metode pembacaan Matriks Anda (1. File ; 2. Keyboard)");
-            System.out.print("Masukkan pilihan Anda : ");
-            pil = input.nextInt();
-            System.out.println("");
-        }
+  }
 
-        // Ini inisiasi Matriks nya gmn ya
-        Matrix Maug = new Matrix(0,0);
-        if (pil == 1) {
-            readFile(Maug);
-        } else if (pil == 2) {
-            writeMatrix(Maug);
-        }
-        System.out.println(Maug);
+  public void menuInterBicubic() {
 
+  }
 
-    }
+  public void menuRegresi() {
 
-    public void menuDeterminan() {
+  }
 
-    }
+  public int tanyaMenuUtama(Scanner scanner) {
+    System.out.println("MENU");
+    System.out.println("1. Sistem Persamaan Linier");
+    System.out.println("2. Determinan");
+    System.out.println("3. Matriks Balikan");
+    System.out.println("4. Interpolasi Polinom");
+    System.out.println("5. Interpolasi Bicubic");
+    System.out.println("6. Regresi Linear Berganda");
+    System.out.println("7. Keluar");
+    System.out.print("Masukkan pilihan menu yang diinginkan : ");
 
-    public void menuMatriksBalikan() {
-
-    }
-
-    public void menuInterPolinom() {
-
-    }
-
-    public void menuInterBicubic() {
-
-    }
-    
-    public void menuRegresi() {
-
-    }
+    return scanner.nextInt();
+  }
 }
 
